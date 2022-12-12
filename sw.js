9d3ed078c4c60bf434730912c53b66c6beb05379
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2022-12-12 23:49:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', e =>{
     console.log("Installed!")
     e.waitUntil(
         caches.open("pwa-cache").then(cache =>{
-            return cache.addAll(['index.html','servicew.js','Images/BG.jpg', 'Images/BG2.jpg','Images/logo.png','Images/logo_144x144.png','logo_512x512.png'])
+            return cache.addAll(['index.html','servicew.js','/Images/BG.jpg', '/Images/BG2.jpg','/Images/logo.png','/Images/logo_144x144.png','/logo_512x512.png'])
         })
     )
 })
